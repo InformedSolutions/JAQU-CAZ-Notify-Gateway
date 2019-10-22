e574fcf30a1b254412c171fcdfa99f077f4314af
Alice King <alice.king@informed.com>
2019-10-22 11:19:53 +0100

    add handling for invalid JSON supplied as part of request

--- src/main/java/uk/gov/caz/notify/controller/ManualDispatchController.java
+++ src/main/java/uk/gov/caz/notify/controller/ManualDispatchController.java
@@ -2,6 +2,8 @@ package uk.gov.caz.notify.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import uk.gov.caz.notify.domain.SendEmailRequest;
 import uk.gov.caz.notify.repository.GovUkNotifyRepository;
 import uk.gov.service.notify.NotificationClientException;
@@ -26,16 +28,27 @@ public class ManualDispatchController {
 
   public ManualDispatchController(GovUkNotifyRepository govUkNotifyRepository) {
     this.govUkNotifyRepository = govUkNotifyRepository;
-
   }
 
-  @GetMapping("/sendEmail")
   @ApiOperation(value = "Manually dispatch an email",
       response = ResponseEntity.class)
-  public SendEmailResponse sendEmail(@RequestBody SendEmailRequest request)
-      throws JsonParseException, JsonMappingException,
-      NotificationClientException, IOException {
-    return govUkNotifyRepository.sendEmail(request.templateId,
-        request.emailAddress, request.personalisation, request.reference);
+  @ApiResponses({
+      @ApiResponse(code = 503, message = "External service unavailable"),
+      @ApiResponse(code = 500, message = "Internal server error"),
+      @ApiResponse(code = 429, message = "Rate limit exceeded"),
+      @ApiResponse(code = 400, message = "Bad request"),
+      @ApiResponse(code = 200, message = "Email sent"),})
+  @GetMapping("/sendEmail")
+  public ResponseEntity<SendEmailResponse> sendEmail(
+      @RequestBody SendEmailRequest request)
+      throws NotificationClientException {
+    try {
+      SendEmailResponse sendEmailResponse =
+          govUkNotifyRepository.sendEmail(request.templateId,
+              request.emailAddress, request.personalisation, request.reference);
+      return ResponseEntity.ok(sendEmailResponse);
+    } catch (IOException e) {
+      return ResponseEntity.badRequest().build();
+    }
   }
 }
--- src/test/java/uk/gov/caz/notify/controller/ManualDispatchControllerTest.java
+++ src/test/java/uk/gov/caz/notify/controller/ManualDispatchControllerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.IOException;
 import java.util.UUID;
 import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -27,19 +28,21 @@ class ManualDispatchControllerTest {
   @Mock
   GovUkNotifyRepository govUkNotifyRepository;
 
-  @Test
-  void canSendEmail() throws JsonParseException, JsonMappingException,
-      NotificationClientException, IOException {
-    SendEmailRequest sendEmailRequest = new SendEmailRequest();
-    String templateId = UUID.randomUUID().toString();
-    String reference = UUID.randomUUID().toString();
-    sendEmailRequest.emailAddress = "test@test.com";
-    sendEmailRequest.templateId = templateId;
-    sendEmailRequest.personalisation = "{}";
-    sendEmailRequest.reference = reference;
+  SendEmailRequest sendEmailRequest;
+  SendEmailResponse sendEmailResponse;
 
+  String response;
+  String templateId;
+  String reference;
 
-    String response = new JSONObject().put("id", UUID.randomUUID().toString())
+  @BeforeEach
+  void init() {
+
+    sendEmailRequest = new SendEmailRequest();
+    templateId = UUID.randomUUID().toString();
+    reference = UUID.randomUUID().toString();
+
+    response = new JSONObject().put("id", UUID.randomUUID().toString())
         .put("reference", reference)
         .put("content",
             (new JSONObject()).put("body", "testbody").put("subject",
@@ -47,17 +50,36 @@ class ManualDispatchControllerTest {
         .put("template", (new JSONObject()).put("id", templateId)
             .put("version", 1).put("uri", "1"))
         .toString();
-    SendEmailResponse sendEmailResponse = new SendEmailResponse(response);
 
+    sendEmailRequest.emailAddress = "test@test.com";
+    sendEmailRequest.templateId = templateId;
+    sendEmailRequest.personalisation = "{}";
+    sendEmailRequest.reference = reference;
+
+    sendEmailResponse = new SendEmailResponse(response);
+  }
+
+  @Test
+  void canSendEmail() throws NotificationClientException, IOException {
     Mockito.when(govUkNotifyRepository.sendEmail(templateId,
         sendEmailRequest.emailAddress, sendEmailRequest.personalisation,
         reference)).thenReturn(sendEmailResponse);
 
-    SendEmailResponse testResponse =
+    ResponseEntity<SendEmailResponse> testResponse =
         manualDispatchController.sendEmail(sendEmailRequest);
-    assertEquals(sendEmailResponse, testResponse);
+    assertEquals(sendEmailResponse, testResponse.getBody());
+  }
 
+  @Test
+  void canGetBadRequestError()
+      throws JsonParseException, NotificationClientException, IOException {
+    Mockito.when(govUkNotifyRepository.sendEmail(templateId,
+        sendEmailRequest.emailAddress, sendEmailRequest.personalisation,
+        reference)).thenThrow(new JsonParseException(null, "bad json"));
 
+    ResponseEntity<SendEmailResponse> testResponse =
+        manualDispatchController.sendEmail(sendEmailRequest);
+    assertEquals(400, testResponse.getStatusCode().value());
   }
 
 }
