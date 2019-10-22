e30c88388756facee5cd85cb9a6765c43a3b9cd4
Alice King <alice.king@informed.com>
2019-10-22 10:26:13 +0100

    integrate gateway with Gov.UK Notify

--- src/main/java/uk/gov/caz/notify/Application.java
+++ src/main/java/uk/gov/caz/notify/Application.java
@@ -4,18 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import uk.gov.caz.notify.configuration.RequestMappingConfiguration;
 import uk.gov.caz.notify.configuration.SwaggerConfiguration;
-import uk.gov.caz.notify.controller.ExampleController;
+import uk.gov.caz.notify.controller.ManualDispatchController;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
-@Import({
-    RequestMappingConfiguration.class,
-    SwaggerConfiguration.class,
-    ExampleController.class
-})
+@ComponentScan({"uk.gov.caz.notify"})
 public class Application extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
---
+++ src/test/java/uk/gov/caz/notify/repository/GovUkNotifyRepositoryTest.java
@@ -0,0 +1,198 @@
+package uk.gov.caz.notify.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.io.IOException;
+import java.util.UUID;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import uk.gov.service.notify.NotificationClient;
+import uk.gov.service.notify.NotificationClientException;
+import uk.gov.service.notify.SendEmailResponse;
+
+@ExtendWith(MockitoExtension.class)
+public class GovUkNotifyRepositoryTest {
+
+  GovUkNotifyRepository govUkNotifyRepository;
+
+  @Mock
+  NotificationClient client;
+
+  String apiKey = "testApiKey";
+  String templateId;
+  String emailAddress = "test@notify.com";
+  String personalisation = "{\"test\": \"test2\"}";
+  String reference;
+  SendEmailResponse sendEmailResponse;
+
+  @BeforeEach
+  void init() {
+    govUkNotifyRepository = new GovUkNotifyRepository(null);
+    ReflectionTestUtils.setField(govUkNotifyRepository, "client", client);
+    reference = UUID.randomUUID().toString();
+    templateId = UUID.randomUUID().toString();
+    String response = new JSONObject().put("id", UUID.randomUUID().toString())
+        .put("reference", reference)
+        .put("content",
+            (new JSONObject()).put("body", "testbody").put("subject",
+                "testsubject"))
+        .put("template", (new JSONObject()).put("id", templateId)
+            .put("version", 1).put("uri", "1"))
+        .toString();
+    sendEmailResponse = new SendEmailResponse(response);
+  }
+
+  /**
+   * Successful email test
+   * 
+   * @throws NotificationClientException
+   * @throws JsonParseException
+   * @throws JsonMappingException
+   * @throws IOException
+   */
+  @Test
+  public void canSendAnEmail() throws NotificationClientException,
+      JsonParseException, JsonMappingException, IOException {
+    Mockito
+        .when(client.sendEmail(Mockito.anyString(), Mockito.anyString(),
+            Mockito.anyMap(), Mockito.anyString()))
+        .thenReturn(sendEmailResponse);
+
+    SendEmailResponse testResponse = govUkNotifyRepository.sendEmail(templateId,
+        emailAddress, personalisation, reference);
+    assertEquals(templateId, testResponse.getTemplateId().toString());
+    assertEquals(reference, testResponse.getReference().get());
+
+  }
+
+  @Test
+  public void jsonParseErrorThrown()
+      throws NotificationClientException, JsonMappingException, IOException {
+    String personalisation = "{\"testJustKey\"}";
+    try {
+      govUkNotifyRepository.sendEmail(templateId, emailAddress, personalisation,
+          reference);
+    } catch (JsonParseException e) {
+      assertNotNull(e);
+    }
+  }
+
+  /**
+   * Bad request error test
+   * 
+   * @throws JsonParseException
+   * @throws JsonMappingException
+   * @throws IOException
+   */
+  @Test
+  public void throwsBadRequestError()
+      throws JsonParseException, JsonMappingException, IOException {
+    NotificationClientException err =
+        new NotificationClientException("Status code: " + 400 + " " + null);
+    try {
+      Mockito.when(client.sendEmail(Mockito.anyString(), Mockito.anyString(),
+          Mockito.anyMap(), Mockito.anyString())).thenThrow(err);
+      govUkNotifyRepository.sendEmail(templateId, emailAddress, personalisation,
+          reference);
+    } catch (NotificationClientException e) {
+      assertEquals("Status code: 400 null", e.getMessage());
+    }
+  }
+
+  /**
+   * Authentication error test
+   * 
+   * @throws JsonParseException
+   * @throws JsonMappingException
+   * @throws IOException
+   */
+  @Test
+  public void throwsAuthError()
+      throws JsonParseException, JsonMappingException, IOException {
+    NotificationClientException err =
+        new NotificationClientException("Status code: " + 403 + " " + null);
+    try {
+      Mockito.when(client.sendEmail(Mockito.anyString(), Mockito.anyString(),
+          Mockito.anyMap(), Mockito.anyString())).thenThrow(err);
+      govUkNotifyRepository.sendEmail(templateId, emailAddress, personalisation,
+          reference);
+    } catch (NotificationClientException e) {
+      assertEquals("Status code: 403 null", e.getMessage());
+    }
+  }
+
+  /**
+   * Too Many Requests error test
+   * 
+   * @throws JsonParseException
+   * @throws JsonMappingException
+   * @throws IOException
+   */
+  @Test
+  public void throwsTooManyRequestsError()
+      throws JsonParseException, JsonMappingException, IOException {
+    NotificationClientException err =
+        new NotificationClientException("Status code: " + 429 + " " + null);
+    try {
+      Mockito.when(client.sendEmail(Mockito.anyString(), Mockito.anyString(),
+          Mockito.anyMap(), Mockito.anyString())).thenThrow(err);
+      govUkNotifyRepository.sendEmail(templateId, emailAddress, personalisation,
+          reference);
+    } catch (NotificationClientException e) {
+      assertEquals("Status code: 429 null", e.getMessage());
+    }
+  }
+
+  /**
+   * Internal Server Error test
+   * 
+   * @throws JsonParseException
+   * @throws JsonMappingException
+   * @throws IOException
+   */
+  @Test
+  public void throwsInternalServerError()
+      throws JsonParseException, JsonMappingException, IOException {
+    NotificationClientException err =
+        new NotificationClientException("Status code: " + 500 + " " + null);
+    try {
+      Mockito.when(client.sendEmail(Mockito.anyString(), Mockito.anyString(),
+          Mockito.anyMap(), Mockito.anyString())).thenThrow(err);
+      govUkNotifyRepository.sendEmail(templateId, emailAddress, personalisation,
+          reference);
+    } catch (NotificationClientException e) {
+      assertEquals("Status code: 500 null", e.getMessage());
+    }
+  }
+
+  /**
+   * Service Unavailable error test
+   * 
+   * @throws JsonParseException
+   * @throws JsonMappingException
+   * @throws IOException
+   */
+  @Test
+  public void throwsServiceDownError()
+      throws JsonParseException, JsonMappingException, IOException {
+    NotificationClientException err =
+        new NotificationClientException("Status code: " + 503 + " " + null);
+    try {
+      Mockito.when(client.sendEmail(Mockito.anyString(), Mockito.anyString(),
+          Mockito.anyMap(), Mockito.anyString())).thenThrow(err);
+      govUkNotifyRepository.sendEmail(templateId, emailAddress, personalisation,
+          reference);
+    } catch (NotificationClientException e) {
+      assertEquals("Status code: 503 null", e.getMessage());
+    }
+  }
+
+}
