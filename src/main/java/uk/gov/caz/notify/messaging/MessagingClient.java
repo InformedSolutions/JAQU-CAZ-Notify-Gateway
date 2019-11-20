749f613acc3501f6c69b4698bc01d957111e9d25
Alice King <alice.king@informed.com>
2019-11-20 13:58:59 +0000

    remove spring cloud libraries and make aws calls synchronous

--- src/main/java/uk/gov/caz/notify/messaging/MessagingClient.java
+++ src/main/java/uk/gov/caz/notify/messaging/MessagingClient.java
@@ -1,12 +1,15 @@
 package uk.gov.caz.notify.messaging;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.cloud.aws.messaging.core.SqsMessageHeaders;
 import org.springframework.stereotype.Component;
 import uk.gov.caz.notify.domain.QueueName;
@@ -21,9 +24,9 @@ import uk.gov.service.notify.NotificationClientException;
 @Slf4j
 public class MessagingClient {
 
-  private final QueueMessagingTemplate messagingTemplate;
-
+  private final AmazonSQS client;
   private final GovUkNotifyWrapper govUkNotifyWrapper;
+  private final ObjectMapper objectMapper;
 
   @Value("${job.notify-gateway.new-queue-url}")
   String newQueue;
@@ -40,38 +43,60 @@ public class MessagingClient {
   @Value("${job.notify-gateway.service-error-queue-url}")
   String serviceErrorQueue;
 
-  public MessagingClient(GovUkNotifyWrapper govUkNotifyWrapper,
-      QueueMessagingTemplate messagingTemplate) {
+  /**
+   * Constructor for the external queuing provider client.
+   * 
+   * @param client a synchronous client for interfacing with Amazon SQS
+   * @param govUkNotifyWrapper a wrapper for the Gov.UK Notify service
+   * @param objectMapper library class for serializing and deserializing JSON
+   */
+  public MessagingClient(AmazonSQS client, GovUkNotifyWrapper govUkNotifyWrapper,
+      ObjectMapper objectMapper) {
+    this.client = client;
     this.govUkNotifyWrapper = govUkNotifyWrapper;
-    this.messagingTemplate = messagingTemplate;
+    this.objectMapper = objectMapper;
   }
 
   /**
    * A method to publish a message to a queue.
    * 
    * @param queueName the queue which the message should be sent to
-   * @param message   the message object
-   * @param headers   attributes that should be sent with the message
+   * @param message the message object
+   * @param messageGroupId an identifier to distinguish payment receipts in a queue
    */
-  public void publishMessage(String queueName, Object message,
-      Map<String, Object> headers) {
-    messagingTemplate.convertAndSend(queueName, message, headers);
+  public void publishMessage(String queueName, String message, String messageGroupId) {
+    SendMessageRequest sendMessageRequest = new SendMessageRequest();
+    UUID messageDeduplicationId = UUID.randomUUID();
+
+    sendMessageRequest.setMessageGroupId(messageGroupId);
+    sendMessageRequest.setMessageDeduplicationId(messageDeduplicationId.toString());
+    sendMessageRequest.putCustomRequestHeader("contentType", "application/json");
+    sendMessageRequest.setQueueUrl(client.getQueueUrl(queueName).getQueueUrl());
+
+    try {
+      sendMessageRequest.setMessageBody(objectMapper.writeValueAsString(message));
+    } catch (JsonProcessingException e) {
+      log.error(e.getMessage());
+    }
+
+    log.info("Sending email message object to SQS queue {} with de-duplication ID: {}", queueName,
+        messageDeduplicationId);
+    client.sendMessage(sendMessageRequest);
   }
 
   /**
-   * Helper method to strip out useful headers and add new headers should a
-   * message need to be sent to a new queue.
+   * Helper method to strip out useful headers and add new headers should a message need to be sent
+   * to a new queue.
    * 
-   * @param  oldHeaders the previous headers to be filtered
-   * @return            new headers
+   * @param oldHeaders the previous headers to be filtered
+   * @return new headers
    */
   public Map<String, Object> filterHeaders(Map<String, String> oldHeaders) {
     Map<String, Object> newHeaders = new HashMap<String, Object>();
 
     String messageGroupId = oldHeaders.get("MessageGroupId");
     newHeaders.put(SqsMessageHeaders.SQS_GROUP_ID_HEADER, messageGroupId);
-    newHeaders.put(SqsMessageHeaders.SQS_DEDUPLICATION_ID_HEADER,
-        UUID.randomUUID().toString());
+    newHeaders.put(SqsMessageHeaders.SQS_DEDUPLICATION_ID_HEADER, UUID.randomUUID().toString());
 
     return newHeaders;
   }
@@ -79,21 +104,18 @@ public class MessagingClient {
   /**
    * Helper method to retry sending the message.
    * 
-   * @param  sendEmailRequest       the key parameters of the email to be sent
-   * @param  i                      the number of attempts to try
-   * @return                        true if message successfully sent, false
-   *                                otherwise
+   * @param sendEmailRequest the key parameters of the email to be sent
+   * @param i the number of attempts to try
+   * @return true if message successfully sent, false otherwise
    * @throws InstantiationException thrown if the API key for Notify is not set
    */
   private boolean retryMessage(SendEmailRequest sendEmailRequest, int i)
       throws InstantiationException {
     while (i > 0) {
-      log.debug("Retrying message with reference: {}",
-          sendEmailRequest.reference);
+      log.info("Retrying message with reference: {}", sendEmailRequest.reference);
       try {
-        govUkNotifyWrapper.sendEmail(sendEmailRequest.templateId,
-            sendEmailRequest.emailAddress, sendEmailRequest.personalisation,
-            sendEmailRequest.reference);
+        govUkNotifyWrapper.sendEmail(sendEmailRequest.templateId, sendEmailRequest.emailAddress,
+            sendEmailRequest.personalisation, sendEmailRequest.reference);
         return true;
       } catch (NotificationClientException | IOException e) {
         i = i - 1;
@@ -104,83 +126,93 @@ public class MessagingClient {
   }
 
   /**
-   * Handles the sending of the email and manages any errors thrown as a result
-   * of this process.
+   * Handles the sending of the email and manages any errors thrown as a result of this process.
    * 
-   * @param  sendEmailRequest       the body of the email request to be sent
-   * @param  newHeaders             the message headers received from SQS
+   * @param sendEmailRequest the body of the email request to be sent
+   * @param messageGroupId the message headers received from SQS
+   * @throws JsonProcessingException thrown if the request cannot be written to a string
    * @throws InstantiationException thrown if the API key for Notify is not set
    */
-  public void handleMessage(SendEmailRequest sendEmailRequest,
-      Map<String, Object> newHeaders) throws InstantiationException {
+  public void handleMessage(SendEmailRequest sendEmailRequest, String messageGroupId)
+      throws JsonProcessingException, InstantiationException {
+    String msgBody = objectMapper.writeValueAsString(sendEmailRequest);
     try {
       log.info("Sending email with reference {}", sendEmailRequest.reference);
-      govUkNotifyWrapper.sendEmail(sendEmailRequest.templateId,
-          sendEmailRequest.emailAddress, sendEmailRequest.personalisation,
-          sendEmailRequest.reference);
-      log.debug("Message successfully sent: {}", sendEmailRequest.reference);
+      govUkNotifyWrapper.sendEmail(sendEmailRequest.templateId, sendEmailRequest.emailAddress,
+          sendEmailRequest.personalisation, sendEmailRequest.reference);
+      log.info("Message successfully sent: {}", sendEmailRequest.reference);
     } catch (NotificationClientException e) {
       int status = e.getHttpResult();
       log.error(e.getMessage());
+      log.info("Got status {} from Notify Gateway for message: {}", status,
+          sendEmailRequest.reference);
 
       boolean success;
 
       switch (status) {
         case 400:
-          publishMessage(deadLetterQueue, sendEmailRequest, newHeaders);
+          log.info("Publishing message with reference {} to the dead letter queue",
+              sendEmailRequest.reference);
+          publishMessage(deadLetterQueue, msgBody, messageGroupId);
           break;
         case 403:
-          publishMessage(deadLetterQueue, sendEmailRequest, newHeaders);
+          log.info("Publishing message with reference {} to the dead letter queue",
+              sendEmailRequest.reference);
+          publishMessage(deadLetterQueue, msgBody, messageGroupId);
           break;
         case 429:
           success = retryMessage(sendEmailRequest, 3);
           if (success) {
-            log.debug("Message successfully sent: {}",
-                sendEmailRequest.reference);
+            log.info("Message successfully sent: {}", sendEmailRequest.reference);
           } else {
-            publishMessage(requestLimitQueue, sendEmailRequest, newHeaders);
+            log.info("Publishing message with reference {} to the request limit queue",
+                sendEmailRequest.reference);
+            publishMessage(requestLimitQueue, msgBody, messageGroupId);
           }
           break;
         case 500:
           success = retryMessage(sendEmailRequest, 3);
           if (success) {
-            log.debug("Message successfully sent: {}",
-                sendEmailRequest.reference);
+            log.info("Message successfully sent: {}", sendEmailRequest.reference);
           } else {
-            publishMessage(serviceErrorQueue, sendEmailRequest, newHeaders);
+            log.info("Publishing message with reference {} to the service error queue",
+                sendEmailRequest.reference);
+            publishMessage(serviceErrorQueue, msgBody, messageGroupId);
           }
           break;
         case 503:
           success = retryMessage(sendEmailRequest, 3);
           if (success) {
-            log.debug("Message successfully sent: {}",
-                sendEmailRequest.reference);
+            log.info("Message successfully sent: {}", sendEmailRequest.reference);
           } else {
-            publishMessage(serviceDownQueue, sendEmailRequest, newHeaders);
+            log.info("Publishing message with reference {} to the service down queue",
+                sendEmailRequest.reference);
+            publishMessage(serviceDownQueue, msgBody, messageGroupId);
           }
           break;
         default:
           success = retryMessage(sendEmailRequest, 3);
           if (success) {
-            log.debug("Message successfully sent: {}",
-                sendEmailRequest.reference);
+            log.info("Message successfully sent: {}", sendEmailRequest.reference);
           } else {
-            publishMessage(deadLetterQueue, sendEmailRequest, newHeaders);
+            log.info("Publishing message with reference {} to the dead letter queue",
+                sendEmailRequest.reference);
+            publishMessage(deadLetterQueue, msgBody, messageGroupId);
           }
           break;
       }
     } catch (IOException e) {
       log.error(e.getMessage());
-      publishMessage(deadLetterQueue, sendEmailRequest, newHeaders);
+      publishMessage(deadLetterQueue, msgBody, messageGroupId);
     }
   }
 
   /**
-   * Helper method to return a full queue name (with consideration for
-   * environment) from the generic name of a queue.
+   * Helper method to return a full queue name (with consideration for environment) from the generic
+   * name of a queue.
    * 
-   * @param  queueName generic name of the queue
-   * @return           full name of queue target
+   * @param queueName generic name of the queue
+   * @return full name of queue target
    */
   public String getEnvQueueName(String queueName) {
     if (queueName.equals(QueueName.NEW.toString())) {
