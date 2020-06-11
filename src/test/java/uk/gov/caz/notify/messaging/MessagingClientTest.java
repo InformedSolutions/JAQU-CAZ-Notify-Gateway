89099d7152d28784ee3377331a9449e6be8398b3
jaroslaw <jaroslaw.piotrowski@pattern-match.com>
2020-06-11 13:15:41 +0200

    CAZB-1646 update code coverage

--- src/test/java/uk/gov/caz/notify/messaging/MessagingClientTest.java
+++ src/test/java/uk/gov/caz/notify/messaging/MessagingClientTest.java
@@ -1,21 +1,26 @@
 package uk.gov.caz.notify.messaging;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.GetQueueUrlResult;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.UUID;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.model.GetQueueUrlResult;
-import com.amazonaws.services.sqs.model.SendMessageRequest;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import uk.gov.caz.notify.dto.SendEmailRequest;
 import uk.gov.caz.notify.repository.GovUkNotifyWrapper;
 import uk.gov.service.notify.NotificationClientException;
@@ -35,18 +40,14 @@ public class MessagingClientTest {
   NotificationClientException err;
 
   String messageGroupId;
-  String message;
   SendEmailRequest sendEmailRequest;
   String emailAddress;
   String templateId;
   String personalisation;
   String reference;
 
-  ObjectMapper om;
-
-
   @BeforeEach
-  void init() throws JsonProcessingException {
+  void init() {
     messagingClient = new MessagingClient(client, govUkNotifyWrapper, new ObjectMapper());
 
     sendEmailRequest = new SendEmailRequest();
@@ -66,16 +67,13 @@ public class MessagingClientTest {
     ReflectionTestUtils.setField(messagingClient, "requestLimitQueue", "requestLimit");
     ReflectionTestUtils.setField(messagingClient, "serviceDownQueue", "serviceDown");
     ReflectionTestUtils.setField(messagingClient, "serviceErrorQueue", "serviceError");
-
-    GetQueueUrlResult getQueueUrlResult = new GetQueueUrlResult();
-    getQueueUrlResult.setQueueUrl("testurl");
-    Mockito.when(client.getQueueUrl(Mockito.anyString())).thenReturn(getQueueUrlResult);
   }
 
   @Test
   void canSendBadRequestErrorToDlq()
       throws NotificationClientException, IOException, InstantiationException {
 
+    mockQueueUrl();
     Mockito.when(err.getHttpResult()).thenReturn(400);
     Mockito.when(err.getMessage()).thenReturn("Error thrown successfully.");
 
@@ -93,6 +91,7 @@ public class MessagingClientTest {
   void canSendAuthorizationErrorToDlq()
       throws NotificationClientException, IOException, InstantiationException {
 
+    mockQueueUrl();
     Mockito.when(err.getHttpResult()).thenReturn(403);
     Mockito.when(err.getMessage()).thenReturn("Error thrown successfully.");
 
@@ -110,6 +109,7 @@ public class MessagingClientTest {
   void canSendRequestLimitErrorToRequestLimitQueue()
       throws NotificationClientException, IOException, InstantiationException {
 
+    mockQueueUrl();
     Mockito.when(err.getHttpResult()).thenReturn(429);
     Mockito.when(err.getMessage()).thenReturn("Error thrown successfully.");
 
@@ -127,6 +127,7 @@ public class MessagingClientTest {
   void canSendServiceErrorToServiceErrorQueue()
       throws NotificationClientException, IOException, InstantiationException {
 
+    mockQueueUrl();
     Mockito.when(err.getHttpResult()).thenReturn(500);
     Mockito.when(err.getMessage()).thenReturn("Error thrown successfully.");
 
@@ -144,6 +145,7 @@ public class MessagingClientTest {
   void canSendServiceDownToServiceDownQueue()
       throws NotificationClientException, IOException, InstantiationException {
 
+    mockQueueUrl();
     Mockito.when(err.getHttpResult()).thenReturn(503);
     Mockito.when(err.getMessage()).thenReturn("Error thrown successfully.");
 
@@ -161,6 +163,7 @@ public class MessagingClientTest {
   void canSendUnexpectedErrorToDlq()
       throws NotificationClientException, IOException, InstantiationException {
 
+    mockQueueUrl();
     Mockito.when(err.getHttpResult()).thenReturn(418);
 
     Mockito.when(govUkNotifyWrapper.sendEmail(templateId, emailAddress, personalisation, reference))
@@ -179,6 +182,7 @@ public class MessagingClientTest {
       throws NotificationClientException, IOException, InstantiationException {
 
     this.sendEmailRequest.setPersonalisation("{");
+    mockQueueUrl();
     IOException err = mock(IOException.class);
     Mockito.when(err.getMessage()).thenReturn("Error thrown successfully.");
 
@@ -192,4 +196,28 @@ public class MessagingClientTest {
     Mockito.verify(client, times(1)).sendMessage(Mockito.any(SendMessageRequest.class));
   }
 
+  @ParameterizedTest
+  @MethodSource("queuesAndFields")
+  void shouldReturnNameOfTheQueue(String queueName, String fieldName, String fieldValue) {
+    ReflectionTestUtils.setField(messagingClient, fieldName, fieldValue);
+
+    String envQueueName = messagingClient.getEnvQueueName(queueName);
+
+    assertThat(envQueueName).isEqualTo(fieldValue);
+  }
+
+  private static Stream<Arguments> queuesAndFields() {
+    return Stream.of(
+        Arguments.of("new", "newQueue", "newQueueName"),
+        Arguments.of("request-limit", "requestLimitQueue", "requestLimitQueueName"),
+        Arguments.of("service-down", "serviceDownQueue", "serviceDownQueueName"),
+        Arguments.of("service-error", "serviceErrorQueue", "serviceErrorQueueName")
+    );
+  }
+
+  private void mockQueueUrl() {
+    GetQueueUrlResult getQueueUrlResult = new GetQueueUrlResult();
+    getQueueUrlResult.setQueueUrl("testurl");
+    Mockito.when(client.getQueueUrl(Mockito.anyString())).thenReturn(getQueueUrlResult);
+  }
 }
